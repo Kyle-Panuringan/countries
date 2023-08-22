421d505cdd6a8e156c88b0616dce9089688078bb
Kyle <kylepanurignan@gmail.com>
2023-08-22 23:20:57 +0900

    Outline Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <>
+    <div id="App">
       <Navbar />
-    </>
+      <Main />
+    </div>
   );
 };
 
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,16 @@
+import "./main.scss";
+
+const Main = () => {
+  return (
+    <div id="main">
+      <div className="grid">
+        <div className="card">a</div>
+        <div className="card">a</div>
+        <div className="card">a</div>
+        <div className="card">a</div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
